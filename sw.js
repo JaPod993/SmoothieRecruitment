cd928d6c17fd5dd74f03868eb368c86930acab1d
Jan Podgorski <j.pod.12@gmail.com>
2019-05-07 20:15:45 +0200

    fixed manifest and service worker scope issue with github pages

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
-console.log('Service worker loaded');
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js');
 
 if (workbox) {
-    console.log('WorkBox working');
 
     workbox.core.setCacheNameDetails({
         prefix: 'SmoothieDelight',
